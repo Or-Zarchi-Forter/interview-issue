1619b68069beb303bfd95126ddcb4abd0a94c096
Or-Zarchi-Forter <87055040+Or-Zarchi-Forter@users.noreply.github.com>
2025-06-16 11:12:03 +0300

    Update redis creds

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const { createClient } = require("./redis-client");
 const express = require("express");
 const PORT = 8000;
 
-const redisClient = createClient('redis://default:4nqD2QiKj2daYdxpJ50BBRaQfQSOJDqd@redis-17389.c15.us-east-1-2.ec2.redns.redis-cloud.com:17389');
+const redisClient = createClient('redis://default:EQLGQAAuaJzZuOJkq10SpzLIjjwZVGCk@redis-12934.c311.eu-central-1-1.ec2.redns.redis-cloud.com:12934');
 
 async function createApp() {
   const app = express();
